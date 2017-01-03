9d3d8b802ac3d4a79c86f1881ca004e0d50e04df
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:24:14 +0530

    fibonacci series

---
+++ fibonacci series.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            num1 = num2 = 1;
+            Console.WriteLine("{0}", num1);
+            while (num2 < 200)
+            {
+                Console.WriteLine(num2);
+                num2 += num1;
+                num1 = num2 - num1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
