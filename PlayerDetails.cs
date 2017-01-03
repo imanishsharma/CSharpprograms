b8e93454ed001db8c1d0389ea0b6a9b1d1f481d8
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:37:46 +0530

    Display tennis player details

---
+++ PlayerDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication	
+{
+    class Player
+    {
+        string playerName;
+        int rank;
+        public void printPlayerDetails()
+        {
+            Console.WriteLine("\n THE DETAILS OF THE TENNIS PLAYER IS:");
+            Console.Write("NAME :");
+            Console.WriteLine(TENNIS_PLAYER_NAME);
+            Console.Write("RANK:");
+            Console.Write(rank);
+        }
+        public void getPlayerDetails()
+        {
+            Console.WriteLine("\n THE DETAILS OF THE TENNIS PLAYER IS:");
+            Console.WriteLine("ENTER TENNIS PLAYER NAME:");
+            playerName = Console.ReadLine();
+            Console.WriteLine("RANK");
+            rank = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
+    
+     
+       
