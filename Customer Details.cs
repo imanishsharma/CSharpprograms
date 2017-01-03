b82390337edab9993e92a68d70c244dc3ea0eb58
Manish Sharma <manisherma81@gmail.com>
2017-01-04 03:32:58 +0530

    Display Customer Details

---
+++ Customer Details.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUSTOMER
+{
+    class CustomerDetails
+    {
+        static void Main(string[] args)
+        {
+            string customername;
+            string address;
+            int phonenumber;
+            
+            
+            
+                Console.WriteLine("ENTER THE NAME OF CUSTOMER");
+                customername = Console.ReadLine();
+                Console.WriteLine("ENTER THE ADDRESS OF CUSTOMER");
+                address = Console.ReadLine();
+                Console.WriteLine("ENTER THE PHONE NUMBER OF CUSTOMER");
+                phonenumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("DETAILS OF CUSTOMER IS:-)");
+                Console.Write("CUSTOMER NAME: ");
+                Console.WriteLine(customername);
+                Console.Write("ADDREES: ");
+                Console.WriteLine(address);
+                Console.Write("PHONE NO. : ");
+                Console.WriteLine(phonenumber);
+                Console.ReadLine();
+            
+                
+        }
+    }
+}
+
+            
+            
+
+           
+         
+            
+            
+                
+            
+               
+
+        
