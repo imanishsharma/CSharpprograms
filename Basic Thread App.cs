69900722b21a03751563f55b0da47462a1cd57f8
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:39:09 +0530

    example of sleep() method

---
+++ Basic Thread App.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApplication4
+{
+    class BasicThreadApp
+    {
+        public static void ChildThreadCall()
+        {
+            Console.WriteLine("Child thread started");
+            int SleepTime =5000;
+            Console.WriteLine("Sleeping for {0} Seconds", SleepTime/1000);
+         Thread.Sleep(SleepTime);
+            Console.WriteLine("Waking Up");
+        }
+        public static void Main()
+        {
+            ThreadStart ChildRef = new ThreadStart(ChildThreadCall);
+            Console.WriteLine("Main -Creating Child Thread");
+            Thread ChildThread = new Thread(ChildRef);
+            ChildThread.Start();
+            Console.WriteLine("Main - Have requested the start of child thread");
+            Console.ReadLine();
+        }
+    }
+}
+
+
