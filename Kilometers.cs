7deeca2b40ef5b54ec9ced14c7190f7863c97ae4
Manish Sharma <manisherma81@gmail.com>
2017-01-04 03:31:16 +0530

    Convert from km  2 cm and m

---
+++ Kilometers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOUR_JOURNALIST
+{
+    class Kilometers
+    {
+        static void Main(string[] args)
+        {
+            int Kilometer;
+            int meters;
+            int centimeters;
+            Console.Write("ENTER KILOMETER :-)");
+            Kilometer = Convert.ToInt32(Console.ReadLine());
+            meters = Kilometer * 1000;
+            centimeters = Kilometer * 100000;
+            Console.WriteLine("METERS IN " + Kilometer + " KM is:" + meters + " meters &  cnetimeters in " + Kilometer + " KM is: " + centimeters+" Centimeters");
+            Console.ReadLine();
+        }
+    }
+}
