de3b63e9f0867c6ac0f4b21a974f1d231cb8d6c3
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:21:45 +0530

    Check even or odd number

---
+++ Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("ENTER THE NUMBER YOU WANT TO CHECK AN EVEN OR ODD NUMBER");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("THE NUMBER IS EVEN");
+            }
+            else
+            {
+                Console.WriteLine("THE NUMBER IS ODD");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
