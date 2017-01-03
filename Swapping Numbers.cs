fc7299bfea1709fe58162ec61b718d585c66a26f
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:34:08 +0530

    Swapping Numbers

---
+++ Swapping Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class SwappingNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int temp;
+            Console.Write("\nEnter the First Number : ");
+            num1 =Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the Second Number : ");
+            num2 =Convert.ToInt32(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.Write("\nAfter Swapping  : ");
+            Console.Write("\nFirst Number : " + NUM_1);
+            Console.Write("\nSecond Number : " + NUM_2);
+            Console.ReadLine();
+        }
+    }
+}
