7449b70a2698a7bd5d8865a2af58317e8cf6832a
Manish Sharma <manisherma81@gmail.com>
2017-01-04 04:34:58 +0530

    Table of 77

---
+++ Table of 77.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Tables
+    {
+        static void Main(string[] args)
+        {
+            int num, i, result;
+            num = 77;
+            i = 1;
+            do
+            {
+                result = num* i;
+                Console.WriteLine("{0} x {1} = {2}", num, i, result);
+                i++;
+            }
+            while (i <= 10);
+
+            Console.ReadLine();
+        }
+    }
+}
+
